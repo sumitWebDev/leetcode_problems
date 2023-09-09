9defaa1151dd9aacdc774536538645a52caa2f78
Sumit kumar Das <dassumit.ece@gmail.com>
2023-09-08 18:22:35 -0600

    Time: 52 ms (60.65%) | Memory: 42.1 MB (34.09%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function(x, y) {
+    let str1InBin = parseInt(x).toString(2);
+    let str2InBin = parseInt(y).toString(2);
+
+    while(str1InBin.length !== str2InBin.length){
+        if(str1InBin.length < str2InBin.length){
+            
+            str1InBin = '0'+str1InBin
+        }
+        else if(str2InBin.length < str1InBin.length) {
+            str2InBin = '0'+str2InBin
+        }
+    }
+    let distance = 0;
+    for(let i =0; i<str2InBin.length ; i++){
+        if(str1InBin[i] !== str2InBin[i]){
+            distance ++
+        }
+    }
+    return distance;
+};
