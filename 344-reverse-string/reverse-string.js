b7eea859cbaec01686ee14f6b93daf509eb592ce
Sumit kumar Das <dassumit.ece@gmail.com>
2023-09-09 02:22:17 -0600

    Time: 81 ms (60.48%) | Memory: 49.5 MB (43.03%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -6,7 +6,6 @@ var reverseString = function(s) {
     let i = 0, j= s.length -1 ;
     while(i<j){
         [s[i],s[j]] = [s[j],s[i]];
-            i++;
-            j--;
+            i++;j--;
     }
 };
